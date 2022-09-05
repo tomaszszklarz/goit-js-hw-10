01595c37519632e3974514a830990b1f3a9b878b
Tomas Szklarz <tomaszszklarz@icloud.com>
2022-09-05 10:34:00 +0200

    update taks 01

--- src/index.js
+++ src/index.js
@@ -3,5 +3,69 @@ import { fetchCountries, fetchCountries } from './fetchCountries';
 import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
 
-
+const inputCountry = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 const DEBOUNCE_DELAY = 300;
+
+const clearContent = () => {
+  countryInfo.innerHTML = '';
+  countryList.innerHTML = '';
+};
+
+const searchCountry = event => {
+  const findCountry = event.target.value.trim();
+  if (!findCountry) {
+    clearContent();
+    return;
+  }
+  fetchCountries(findCountry)
+    .then(country => {
+      if (country.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        clearContent();
+        return;
+      } else if (country.length === 1) {
+        clearContent(countryList.innerHTML);
+        renderCountryInfo(country);
+      } else if (country.length > 1 && country.length <= 10) {
+        clearContent(countryInfo.innerHTML);
+        renderCountryList(country);
+      }
+    })
+    .catch(error => {
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+      clearContent();
+      return error;
+    });
+};
+
+
+const renderCountryList = country => {
+  const markup = country
+    .map(({ name, flags }) => {
+      return `<li><img src="${flags.svg}" alt="${name.official}" width="100" height="60"><p>${name.official}</p></li>`;
+    })
+    .join('');
+  countryList.innerHTML = markup;
+};
+
+
+const renderCountryInfo = country => {
+  const markup = country
+    .map(({ name, capital, population, flags, languages }) => {
+      return `<section><h1><img src="${flags.svg}" alt="${
+        name.official
+      }" width="100" height="60">&nbsp ${name.official}</h1>
+      <p><span>Capital: </span>&nbsp ${capital}</p>
+      <p><span>Population:</span>&nbsp ${population}</p>
+      <p><span>Languages:</span>&nbsp ${Object.values(languages)}</p><section>`;
+    })
+    .join('');
+  countryInfo.innerHTML = markup;
+};
+
+
+inputCountry.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));

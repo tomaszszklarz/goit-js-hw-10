76b1c56c932e2282eb241eb5e90b4e0d4f0239b2
Tomas Szklarz <tomaszszklarz@icloud.com>
2022-09-05 08:48:44 +0200

    update taks 01

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './css/styles.css';
+import { fetchCountries, fetchCountries } from './fetchCountries';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+
 
 const DEBOUNCE_DELAY = 300;
